9eaad83db42aa7c0f379777a4750bb2d42cdb9b2
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-11-30 00:35:27 +0200

    renew

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -60,7 +60,9 @@ class ContactForm extends Component {
                     <Input
                         type="tel"
                         name="number"
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
                         value= {number}
